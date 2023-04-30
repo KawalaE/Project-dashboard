d16202e1f3a3012cfb0615b7add134af99294caf
KawalaE <edyta.wer.kawala@gmail.com>
2023-04-30 18:06:36 +0200

    Use prettier to edit code

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const project = document.getElementById('project');
-const cardsList = document.querySelectorAll('.card');
-const educationIcon = document.getElementById('edu-info');
-const educationInfo = document.querySelector('.education');
-const contactIcon = document.getElementById('contact-info');
-const contactInfo = document.querySelector('.contact');
-const emailInfo = document.querySelector('#email');
+const project = document.getElementById("project");
+const cardsList = document.querySelectorAll(".card");
+const educationIcon = document.getElementById("edu-info");
+const educationInfo = document.querySelector(".education");
+const contactIcon = document.getElementById("contact-info");
+const contactInfo = document.querySelector(".contact");
+const emailInfo = document.querySelector("#email");
 
-project.addEventListener('mouseover', () => {
+project.addEventListener("mouseover", () => {
   cardsList.forEach((card) => {
-    card.classList.add('transform');
+    card.classList.add("transform");
   });
 });
 
-project.addEventListener('mouseleave', () => {
+project.addEventListener("mouseleave", () => {
   cardsList.forEach((card) => {
-    card.classList.remove('transform');
+    card.classList.remove("transform");
   });
 });
 
-educationIcon.addEventListener('mouseover', () => {
-  educationInfo.classList.add('transform');
+educationIcon.addEventListener("mouseover", () => {
+  educationInfo.classList.add("transform");
 });
 
-educationIcon.addEventListener('mouseleave', () => {
-  educationInfo.classList.remove('transform');
+educationIcon.addEventListener("mouseleave", () => {
+  educationInfo.classList.remove("transform");
 });
 
-contactIcon.addEventListener('mouseover', () => {
-  contactInfo.classList.add('transform');
+contactIcon.addEventListener("mouseover", () => {
+  contactInfo.classList.add("transform");
 });
 
-contactIcon.addEventListener('mouseleave', () => {
-  contactInfo.classList.remove('transform');
+contactIcon.addEventListener("mouseleave", () => {
+  contactInfo.classList.remove("transform");
 });
 
-emailInfo.addEventListener('click', () => {
-  emailInfo.textContent = 'edyta.wer.kawala@gmail.com';
+emailInfo.addEventListener("click", () => {
+  emailInfo.textContent = "edyta.wer.kawala@gmail.com";
 });
