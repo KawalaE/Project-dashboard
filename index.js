3a6e3832d890d71bd035a9969e4d02e335ac3f2e
KawalaE <edyta.wer.kawala@gmail.com>
2023-05-01 13:01:53 +0200

    Fix layout

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const project = document.getElementById("project");
 const cardsList = document.querySelectorAll(".card");
 const educationIcon = document.getElementById("edu-info");
 const educationInfo = document.querySelector(".education");
-const email = document.querySelector(".contact-email");
-const copyStatus = document.querySelector(".copy-status");
 
 project.addEventListener("mouseover", () => {
   cardsList.forEach((card) => {
@@ -25,14 +23,3 @@ educationIcon.addEventListener("mouseleave", () => {
   educationInfo.classList.remove("transform");
 });
 
-email.addEventListener("click", async () => {
-  try {
-    await navigator.clipboard.writeText("edyta.wer.kawala@gmail.com");
-    copyStatus.textContent = "Copied!";
-  } catch (err) {
-    console.log("Failed to copy:", err);
-  }
-  setTimeout(() => {
-    copyStatus.textContent = "";
-  }, "1500");
-});
